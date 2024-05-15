de79e2e622a0281b6b1db1313fc9b95c35e43adc
janak <niraulajanak2019@gmail.com>
2024-05-15 02:51:58 +0000

    Settings: Revamp about phone

--- src/com/android/settings/deviceinfo/aboutphone/EverestInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/aboutphone/EverestInfoPreferenceController.java
@@ -21,7 +21,9 @@ package com.android.settings.deviceinfo.aboutphone;
 import android.content.Context;
 import android.os.SystemProperties;
 import android.widget.TextView;
-import androidx.preference.PreferenceFragmentCompat;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
@@ -32,6 +34,10 @@ import com.android.settingslib.widget.LayoutPreference;
 public class EverestInfoPreferenceController extends AbstractPreferenceController {
 
     private static final String KEY_EVEREST_INFO = "everest_info";
+    private static final String KEY_STORAGE = "storage";
+    private static final String KEY_CHIPSET = "chipset";
+    private static final String KEY_BATTERY = "battery";
+    private static final String KEY_DISPLAY = "display";
 
     public EverestInfoPreferenceController(Context context) {
         super(context);
@@ -43,17 +49,19 @@ public class EverestInfoPreferenceController extends AbstractPreferenceControlle
         final LayoutPreference everestInfoPreference = screen.findPreference(KEY_EVEREST_INFO);
 
         if (everestInfoPreference != null) {
-            final TextView processor = everestInfoPreference.findViewById(R.id.processor_message);
-            final TextView storageAndRAM = everestInfoPreference.findViewById(R.id.storage_code_message);
-            final TextView battery = everestInfoPreference.findViewById(R.id.battery_type_message);
-            final TextView infoScreen = everestInfoPreference.findViewById(R.id.screen_message);
+            final TextView processor = everestInfoPreference.findViewById(R.id.chipset_summary);
+            final TextView storageAndRAM = everestInfoPreference.findViewById(R.id.cust_storage_summary);
+            final TextView battery = everestInfoPreference.findViewById(R.id.cust_battery_summary);
+            final TextView infoScreen = everestInfoPreference.findViewById(R.id.cust_display_summary);
 
+            if (processor != null && storageAndRAM != null && battery != null && infoScreen != null) {
             Context context = everestInfoPreference.getContext();
 
             processor.setText(EverestSpecUtils.getProcessorModel(context));
             storageAndRAM.setText(EverestSpecUtils.getStorageAndRAMInfo(context));
             battery.setText(EverestSpecUtils.getBatteryInfo(context));
             infoScreen.setText(EverestSpecUtils.getScreenRes(context));
+            }
         }
     }
 
